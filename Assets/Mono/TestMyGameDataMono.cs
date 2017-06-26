79ca3c564f9bc2f5d27fb933e1fcbeab523f17fa
Star <starlove124760@gmail.com>
2017-06-26 16:19:26 +0800

    First Commit. 相關範例檔案

---
+++ Assets/Mono/TestMyGameDataMono.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMyGameDataMono : MonoBehaviour {
+    public MyGameData m_MyGameData;
+    // Use this for initialization
+    void Start () {
+        print( m_MyGameData.Speed );
+        print( m_MyGameData.Time );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SampleClass/MyGameData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameData : ScriptableObject
+{
+    public int Speed;
+    public float Time;
+}
